07cbb1d3d3dcae2dd41ae76c5b28047144f73c6d
Sondre Bakke <sondremartin@hotmail.com>
2025-02-23 15:51:36 +0100

    Litt farger

--- src/newProgram.ts
+++ src/newProgram.ts
@@ -6,6 +6,8 @@ import { bubbleSort, VisualArray } from "./VisualArray";
 
 export function run(p5: P5) {
   let array: number[];
+  const swapped = new Set<number>();
+  const gotten = new Set<number>();
   p5.setup = () => {
     p5.createCanvas(800, 500);
 
@@ -25,11 +27,19 @@ export function run(p5: P5) {
     pubsub.subscribe("set", async ({ payload }) => {
       const { index } = payload;
       const frequency = frequencyMapper(array[index]);
+      swapped.add(index);
+      audioManager.play({ frequency, durationMs: 10 });
+    pubsub.subscribe("get", async ({ payload }) => {
+      const { index } = payload;
+      const frequency = frequencyMapper(array[index]);
+      gotten.add(index);
       audioManager.play({ frequency, durationMs: 10 });
     });
 
     pubsub.subscribe("swap", async ({ payload }) => {
       const { i, j } = payload;
+      swapped.add(i);
+      swapped.add(j);
       audioManager.play({
         frequency: frequencyMapper(array[i]),
         durationMs: 10,
@@ -46,9 +56,21 @@ export function run(p5: P5) {
   p5.draw = () => {
     p5.background(0);
     const width = p5.width / array.length;
-    p5.fill(255);
     array.forEach((value, i) => {
-      p5.rect(i * width, p5.height, width, -value * 5);
+      p5.fill(255);
+      if (swapped.has(i)) {
+        p5.fill(255, 0, 0);
+        swapped.delete(i);
+      } else if (gotten.has(i)) {
+        p5.fill(0, 255, 0);
+        gotten.delete(i);
+      }
+      p5.rect(
+        i * width,
+        p5.height,
+        width,
+        p5.map(value, 0, array.length, 0, -p5.height)
+      );
     });
   };
 }
