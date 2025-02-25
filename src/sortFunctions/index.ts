1334b608f1170be5f8bf0f0d0aa912cd1c478d40
Sondre Bakke <sondremartin@hotmail.com>
2025-02-25 22:27:45 +0100

    l u f t

--- src/sortFunctions/index.ts
+++ src/sortFunctions/index.ts
@@ -2,8 +2,11 @@ import { VisualArray } from "../visualArray";
 import { sortFunctions } from "./_registerSortFunctions";
 
 export type SortFunction = (array: VisualArray) => Promise<void>;
+
 export type SortFunctionName = keyof typeof sortFunctions;
+
 export { sortFunctions } from "./_registerSortFunctions";
+
 export function isSortFunctionName(name: string): name is SortFunctionName {
   return name in sortFunctions;
 }
